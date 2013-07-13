798a39a5220afd4c7d55b6d01239738b3da361cb
Michael Meyer <m.meyer2k@gmail.com>
2013-07-13 16:56:24 -0500

    Update longmath.php

--- longmath.php
+++ longmath.php
@@ -1,7 +1,9 @@
 <?php
 
-#var_dump($argv);
+# include the longmath class
+require_once 'class.longmath.php';
 
+# begin handling main option flags
 if ($argv[1] === '-h' || $argv[1] === '--help'):
     echo '
 Long math options:
@@ -9,7 +11,16 @@ Long math options:
     -a, --add              Add 2 numbers (php longmath.php -a 1234 5678)
     -c, --compare          Subtract one number from another
     -m, --multiply         Multiply 2 numbers
+    -d, --divide           Divide one number out of another
+    -p, --power            Raise one number to the power of another
 ';
+
 elseif ($argv[1] === '-a' || $argv[1] === '--add'):
+    echo longmath::add($argv[2], $argv[3]);
+
+elseif ($argv[1] === '-c' || $argv[1] === '--compare'):
+    echo longmath::compare($argv[2], $argv[3]);
+
+elseif ($argv[1] === '-i' || $argv[1] === '--interactive'):
     
 endif;
