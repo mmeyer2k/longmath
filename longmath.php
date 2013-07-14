0da98d704c2978b2345dbdd6d8804a4df8b952a0
Michael Meyer <m.meyer2k@gmail.com>
2013-07-14 09:27:21 -0500

    Update longmath.php

--- longmath.php
+++ longmath.php
@@ -6,13 +6,16 @@ require_once 'class.longmath.php';
 # begin handling main option flags
 if ($argv[1] === '-h' || $argv[1] === '--help'):
     echo '
-Long math options:
+Longmath, a PHP library for arbitrary precision mathematics
+https://github.com/mmeyer2k/longmath
+Command line options:
     -i, --interactive      Interactive mode
     -a, --add              Add 2 numbers (php longmath.php -a 1234 5678)
     -c, --compare          Subtract one number from another
     -m, --multiply         Multiply 2 numbers
     -d, --divide           Divide one number out of another
     -p, --power            Raise one number to the power of another
+    -z, --collatz          Test the Collatz Conjecture on a given large number
 ';
 
 elseif ($argv[1] === '-a' || $argv[1] === '--add'):
