cc493bbc713f212d9d2681d4ea39a99ed2a4cbe6
Michael Meyer <m.meyer2k@gmail.com>
2013-07-14 16:17:16 -0500

    Update longmath.php

--- longmath.php
+++ longmath.php
@@ -3,18 +3,28 @@
 # include the longmath class
 require_once 'class.longmath.php';
 
+$benchmark = in_array('-b', $argv) || in_array('--benchmark', $argv);
+
+$start = microtime();
+
 # begin handling main option flags
 if ($argv[1] === '-h' || $argv[1] === '--help'):
     echo '
 Longmath, a PHP library for arbitrary precision mathematics
 https://github.com/mmeyer2k/longmath
-Command line options:
-    -i, --interactive      Interactive mode
+
+Usage: php longmath.php operation arg1 [arg2] [optional flags]
+
+Mathematic operations:
     -a, --add              Add 2 numbers (php longmath.php -a 1234 5678)
     -c, --compare          Subtract one number from another
     -m, --multiply         Multiply 2 numbers
     -d, --divide           Divide one number out of another
     -p, --power            Raise one number to the power of another
+
+Other options:
+    -i, --interactive      Interactive mode
+    -b, --benchmark        Return the elapsed time on the line after the result
     -z, --collatz          Test the Collatz Conjecture on a given large number
 ';
 
@@ -27,8 +37,15 @@ elseif ($argv[1] === '-c' || $argv[1] === '--compare'):
 elseif ($argv[1] === '-m' || $argv[1] === '--multiply'):
     echo longmath::multiply($argv[2], $argv[3]);
 
+elseif ($argv[1] === '-d' || $argv[1] === '--divide'):
+    echo longmath::divide($argv[2], $argv[3]);
+
 elseif ($argv[1] === '-i' || $argv[1] === '--interactive'):
 
 else:
-    
+
+endif;
+
+if ($benchmark):
+    echo PHP_EOL . 'Elapsed time: ' . (microtime() - $start) . ' second(s)';
 endif;
